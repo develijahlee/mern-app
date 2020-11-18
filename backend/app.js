5b24c756999b6b75fa6a3c7521970b5d817044e6
Elijah Lee <develijahlee@gmail.com>
2020-11-18 14:15:57 +0900

    add unit tests and CRUD app

--- backend/app.js
+++ backend/app.js
@@ -2,5 +2,29 @@ class MessageApp {
   constructor() {
     this.messages = [];
   }
+  // Create
+  post(content) {
+    let item = {
+      id: this.messages.length,
+      content: content,
+      date: new Date(),
+    };
+    this.messages.push(item);
+    return this.messages;
+  }
+  // Read
+  get(id) {
+    return this.messages[id];
+  }
+  // Update
+  update(id, update) {
+    this.messages[id].content = update;
+    return this.messages[id];
+  }
+  // Delete
+  delete(id) {
+    this.messages.splice(id - 1, 1);
+    return this.messages;
+  }
 }
 export default MessageApp;
