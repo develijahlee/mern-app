c3b768191f6b6445e6adcca44048977e0123ea21
Elijah Lee <develijahlee@gmail.com>
2020-11-21 20:22:44 +0900

    connect express to json data

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,10 @@
 import express from 'express';
+import routes from './lib/routes.js';
 const app = express();
-import bodyParser from 'body-parser';
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-app.use(require('./lib/routes.js'));
+app.use(routes);
 
 app.listen(3001, function () {
   console.log('Connected');
