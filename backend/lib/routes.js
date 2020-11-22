7f3637b362113f17d735aa0865e2d467645ad876
Elijah Lee <develijahlee@gmail.com>
2020-11-22 14:09:27 +0900

    deleteMessage fails

--- backend/lib/routes.js
+++ backend/lib/routes.js
@@ -18,4 +18,13 @@ router.post('/message', async (req, res) => {
     .catch((err) => res.status(404).json(err));
 });
 
+router.delete('/delete/:id', async (req, res) => {
+  await messageApp
+    .deleteMessage(req.params.id)
+    .then((messages) => {
+      res.json(messages);
+    })
+    .catch((err) => res.status(404).json(err));
+});
+
 export default router;
--- backend/test/app.test.js
+++ backend/test/app.test.js
@@ -29,4 +29,16 @@ describe('MessageApp Test', function () {
       done();
     });
   });
+  it('deletes a message', function (done) {
+    const res = request(MessageApp)
+      .delete('/delete/1')
+      .set('Accept', 'application/json');
+    res.expect(200).end(function (err, res) {
+      if (err) {
+        return done(err);
+      }
+      expect(res.body.length).to.equal(0);
+      done();
+    });
+  });
 });
