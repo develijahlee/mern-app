b871715e4c949eee0a486a1d6c679e0eb8ed211b
Elijah Lee <develijahlee@gmail.com>
2020-11-23 15:28:35 +0900

    cors

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import routes from './lib/routes.js';
+import cors from 'cors';
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 app.use(routes);
 
 app.listen(3001, function () {
