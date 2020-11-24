2c7b17c775ff5de138f7c0456c64e457b73cc06e
Elijah Lee <develijahlee@gmail.com>
2020-11-24 22:03:38 +0900

    post and display messages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,11 +4,30 @@ import MessageForm from './components/messageForm.js';
 import axios from 'axios';
 const PORT = 'http://localhost:3001';
 class MessageApp extends Component {
-  submitMessage = (data) => {
-    axios.post(`${PORT}/message`, {
-      content: data,
+  constructor() {
+    super();
+    this.state = {
+      messages: [],
+    };
+  }
+  componentDidMount() {
+    this.getAllMessages();
+  }
+  getAllMessages = () => {
+    axios.get(`${PORT}/`).then((result) => {
+      this.setState({
+        messages: result.data,
+      });
     });
   };
+  submitMessage = (data) => {
+    axios
+      .post(`${PORT}/message`, {
+        content: data,
+      })
+      .then(() => this.getAllMessages());
+  };
+
   render() {
     return (
       <div>
--- frontend/src/components/messageList.js
+++ frontend/src/components/messageList.js
@@ -1,11 +1,23 @@
-import React from 'react';
-class MessageList extends React.Component {
+import React, { Component } from 'react';
+class MessageList extends Component {
   render() {
-    return (
-      <ul id="message_list">
-        <li>message</li>
-      </ul>
-    );
+    if (this.props.messages) {
+      return (
+        <ul id="message_list">
+          {this.props.messages.map((message) => {
+            return (
+              <li className="message" key={message.id}>
+                {message.content}
+                <br />
+                {message.date}
+              </li>
+            );
+          })}
+        </ul>
+      );
+    } else {
+      return <ul id="message_list">no messages</ul>;
+    }
   }
 }
 export default MessageList;
