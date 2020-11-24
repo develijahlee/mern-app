d957c956cca603fbb3c10cf91884d38d2dcc3410
Elijah Lee <develijahlee@gmail.com>
2020-11-24 14:28:23 +0900

    add frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+function MessageApp() {
+  return (
+    <div className="App">
+      <textarea id="message_box"></textarea>
+      <br />
+      <button type="submit" name="Submit" id="submit">
+        Submit
+      </button>
+      <ul id="message_list">message</ul>
+    </div>
+  );
+}
+export default MessageApp;
