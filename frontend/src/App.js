7672576660438821ccd9018987202480b70d60b5
Elijah Lee <develijahlee@gmail.com>
2020-11-24 21:42:43 +0900

    messageForm posts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import MessageList from './components/messageList.js';
 import MessageForm from './components/messageForm.js';
-import './App.css';
-class MessageApp extends React.Component {
+import axios from 'axios';
+const PORT = 'http://localhost:3001';
+class MessageApp extends Component {
+  submitMessage = (data) => {
+    axios.post(`${PORT}/message`, {
+      content: data,
+    });
+  };
   render() {
     return (
-      <div className="App">
-        <MessageForm />
+      <div>
+        <MessageForm ref="messageFormRef" submitMessage={this.submitMessage} />
         <MessageList />
       </div>
     );
